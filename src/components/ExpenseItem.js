0e86b8e6901d35157968ff8790488bb14f0de588
LapertJoseph <lapertjoseph@gmail.com>
2022-01-12 10:14:57 +0100

    Ajout du fichier ExpenseItem.Css et ajout de commentaire dans les notes

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,14 @@
 
+
+
 function ExpenseItem() {
-    return <h2>Expense Item in there!</h2>
+  return (
+    <div>
+      <div>March 28th 2021</div>
+      <div><h2>Car insurance</h2></div>
+      <div>$294.64</div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
