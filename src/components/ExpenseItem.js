3a2d013250a9d270a9abca2b5a35b4682d283a19
LapertJoseph <lapertjoseph@gmail.com>
2022-01-12 11:55:44 +0100

    modif du ExpenseItem.js et docs React

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,20 +1,16 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-
-    const expenseDate= new Date(2021, 2, 28);
-    const expenseTitle= 'Car Insurance';
-    const expenseAmount= 294.67;
+function ExpenseItem(props) {
 
 
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
         <div>
-          <h2>{expenseTitle}</h2>
+          <h2>{props.title}</h2>
         </div>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <div className="expense-item__price">${props.amout}</div>
       </div>
     </div>
   );
