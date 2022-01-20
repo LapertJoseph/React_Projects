11d66f946f99f3177a43dd6064bcc68cc9528065
LapertJoseph <lapertjoseph@gmail.com>
2022-01-20 21:55:23 +0100

    Ajout notes sur l'execution de components React

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,18 +4,21 @@ import Card from '../UI/Card'
 
 const ExpenseItem = (props) => {
 
+  let title = props.title;
+
   const clickHandler = () => {
-    console.log("Click");
+    title = "updated";
+    console.log(title);
   }
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-          <h2>{props.title}</h2>
+          <h2>{title}</h2>
         <div className="expense-item__price">${props.amout}</div>
       </div>
-      <button onClick={clickHandler}>Change</button>             
+      <button onClick={clickHandler}>Change Title</button>             
     </Card>
   );
 }
